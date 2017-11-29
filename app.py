32fcfa06494d355e5e87b95c030dbd411fd9b0ee
shigarus <shigarus@rambler.ru>
2017-11-29 12:43:38 +0300

    Refactor: servers now only responsible for managing connections

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import logging
+from typing import Callable
 
-import tornado.iostream
+from tornado.iostream import StreamClosedError, IOStream
 from tornado.ioloop import IOLoop
 from tornado.tcpserver import TCPServer
 
@@ -10,45 +11,77 @@ import config
 class Application:
 
     def __init__(self):
-        self._sources_server = SourcesServer(self._on_source_msg)
-        self._listeners_server = ListenersServer()
+        self._sources_server = SourcesServer(
+            on_connect=self._on_source_connect,
+            on_msg=self._on_source_msg,
+            on_close=self._on_source_close,
+        )
+        self._listeners_server = ListenersServer(
+            on_connect=self._on_listener_connect,
+            on_close=self._on_listener_close,
+        )
 
     def listen(self, sources_port, listeners_port):
         self._sources_server.listen(sources_port)
         self._listeners_server.listen(listeners_port)
 
-    def _on_source_msg(self, source, msg):
+    def _on_source_connect(self, source: IOStream):
+        pass
+
+    def _on_source_msg(self, source: IOStream, msg: IOStream):
+        pass
+
+    def _on_source_close(self, source: IOStream):
+        pass
+
+    def _on_listener_connect(self, listener: IOStream):
+        pass
+
+    def _on_listener_close(self, listener: IOStream):
         pass
 
 
 class SourcesServer(TCPServer):
+    """
+    Manages connects to sources.
+    """
 
-    def __init__(self, on_msg, *args, **kwargs):
+    def __init__(self,
+                 on_connect: Callable[[IOStream]],
+                 on_msg: Callable[[IOStream, bytes]],
+                 on_close: Callable[[IOStream]],
+                 *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self._on_connect = on_connect
         self._on_msg = on_msg
+        self._on_close = on_close
 
-    async def handle_stream(self, stream, address):
+    async def handle_stream(self, stream: IOStream, address: str):
+        self._on_connect(IOStream)
         try:
             while True:
-                msg = await stream.read_until_close()
+                msg: bytes = await stream.read_until_close()
                 await self._on_msg(stream, msg)
-        except tornado.iostream.StreamClosedError:
-            pass
+        except StreamClosedError:
+            self._on_close(stream)
 
 
 class ListenersServer(TCPServer):
+    """
+    Manages connects to listeners.
+    """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self,
+                 on_connect: Callable[[IOStream]],
+                 on_close: Callable[[IOStream]],
+                 *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._listeners = []
+        self._on_connect = on_connect
+        self._on_close = on_close
 
-    def handle_stream(self, stream, address):
-        self._listeners.append(stream)
-        stream.set_close_callback(lambda: self._listeners.remove(stream))
-
-    async def send_msg_to_all(self, msg):
-        for listener in tuple(self._listeners):
-            await listener.write(msg)
+    def handle_stream(self, stream: IOStream, address: str):
+        self._on_connect(stream)
+        stream.set_close_callback(lambda: self._on_close(stream))
 
 
 def main():
